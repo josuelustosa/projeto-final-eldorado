a3cca1a3945b92f261b31d82a300967c578ba863
Josué Lustosa | Bacharel em de Sistemas de Informação <93569079+josuelustosadev@users.noreply.github.com>
2022-04-12 19:19:52 -0400

    Editing the table creation sequence

--- back-end/scripts-db/db_proj_final_eldorado.sql
+++ back-end/scripts-db/db_proj_final_eldorado.sql
@@ -3,6 +3,14 @@ create database proj_final_eldorado;
 
 /* ------------------------------------------------ */
 
+/* criando a tabela category*/
+CREATE TABLE category (
+	cat_id INT AUTO_INCREMENT PRIMARY KEY,
+    cat_name VARCHAR(128) NOT NULL
+);
+
+/* ------------------------------------------------ */
+
 /* criando a tabela device*/
 CREATE TABLE device (
 	dev_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,12 +18,4 @@ CREATE TABLE device (
     dev_color VARCHAR(16),
     dev_partNumber INT,
     FOREIGN KEY (dev_category_id) REFERENCES category (cat_id)
-);
-
-/* ------------------------------------------------ */
-
-/* criando a tabela category*/
-CREATE TABLE category (
-	cat_id INT AUTO_INCREMENT PRIMARY KEY,
-    cat_name VARCHAR(128) NOT NULL
 );
